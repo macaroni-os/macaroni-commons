a43c7cbe36da0eae38e299c616d8179f34605d3e
Daniele Rondina <geaaru@funtoo.org>
2022-05-26 14:09:10 +0200

    kernel-5.18/*: Fix version generated

--- packages/kernels/5.18/macaroni/build.sh
+++ packages/kernels/5.18/macaroni/build.sh
@@ -3,6 +3,11 @@ PACKAGE_VERSION=${PACKAGE_VERSION%\+*}
 mkdir -p output/boot
 pushd ${KERNEL_TYPE}
 
+minor=$(echo $RELEASE | cut -d'.' -f 3)
+if [ "${minor}" == "" ] ; then
+  PACKAGE_VERSION="${PACKAGE_VERSION}.0"
+fi
+
 make -j$(nproc --ignore=1) KBUILD_BUILD_VERSION="$PACKAGE_VERSION-Macaroni"
 
 if [[ -L "arch/${ARCH}/boot/bzImage" ]]; then
