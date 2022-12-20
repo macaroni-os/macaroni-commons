084acff5b2ac103876372390d6d8cdcb27eb41f7
Daniele Rondina <geaaru@funtoo.org>
2022-12-20 23:31:25 +0100

    kernel-6.0/macaroni-full: Bump v.6.0.14

---
+++ packages/kernels/6.0/macaroni/prepare.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+set -ex
+PACKAGE_VERSION=${PACKAGE_VERSION%\+*}
+wget https://cdn.kernel.org/pub/linux/kernel/v${PACKAGE_VERSION:0:1}.x/${KERNEL_TYPE}-${PACKAGE_VERSION}.tar.xz -O kernel.tar.xz
+tar xvJf kernel.tar.xz
+mv ${KERNEL_TYPE}-${PACKAGE_VERSION} ${KERNEL_TYPE}
+cp -rfv macaroni-$ARCH.config ${KERNEL_TYPE}/.config
+cd ${KERNEL_TYPE}
+make olddefconfig
