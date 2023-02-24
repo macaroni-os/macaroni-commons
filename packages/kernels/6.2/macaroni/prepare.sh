4a3b0c65325891ff9284fcdbf6bf89568b7fc489
Daniele Rondina <geaaru@funtoo.org>
2023-02-24 01:05:49 +0100

    kernel-6.2/macaroni-full: Bump v.6.2.0+1

--- packages/kernels/6.2/macaroni/prepare.sh
+++ packages/kernels/6.2/macaroni/prepare.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 set -ex
 PACKAGE_VERSION=${PACKAGE_VERSION%\+*}
+if [ "${PACKAGE_VERSION}" = "6.2.0" ] ; then
+  PACKAGE_VERSION="6.2"
+fi
 wget https://cdn.kernel.org/pub/linux/kernel/v${PACKAGE_VERSION:0:1}.x/${KERNEL_TYPE}-${PACKAGE_VERSION}.tar.xz -O kernel.tar.xz
 tar xJf kernel.tar.xz
 mv ${KERNEL_TYPE}-${PACKAGE_VERSION} ${KERNEL_TYPE}
