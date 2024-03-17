5338e41aa5f5782ed8b0f31d279b9d0927473692
Daniele Rondina <geaaru@gmail.com>
2024-03-17 19:16:33 +0100

    kernel-6.7/macaroni-zen-sources: Bump v.6.7.6+1

--- packages/kernels/6.7/macaroni-zen/sources/copy.sh
+++ packages/kernels/6.7/macaroni-zen/sources/copy.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 set -ex
 PACKAGE_VERSION=${PACKAGE_VERSION%\+*}
+kernel_version="${PACKAGE_VERSION}-zen1"
 mkdir -p /luetbuild/sources/usr/src/
-mv ${KERNEL_TYPE} /luetbuild/sources/usr/src/${KERNEL_TYPE}-${PACKAGE_VERSION}-${SUFFIX}
+mv ${KERNEL_TYPE} /luetbuild/sources/usr/src/${KERNEL_TYPE}-${kernel_version}-${SUFFIX}
