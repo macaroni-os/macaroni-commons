0070b76f2a6f2b32fef59bdb93898e7ad5f45bc9
Daniele Rondina <geaaru@gmail.com>
2024-03-17 19:16:33 +0100

    kernel-6.7/macaroni-zen-full: Bump v.6.7.9+2

--- packages/kernels/6.7/macaroni-zen/build.sh
+++ packages/kernels/6.7/macaroni-zen/build.sh
@@ -5,8 +5,10 @@ pushd ${KERNEL_TYPE}
 
 make -j$(nproc --ignore=1) KBUILD_BUILD_VERSION="$PACKAGE_VERSION-Macaroni"
 
+kernel_version="${PACKAGE_VERSION}-zen1"
+
 if [[ -L "arch/${ARCH}/boot/bzImage" ]]; then
-   cp -rfv $(readlink -f "arch/${ARCH}/boot/bzImage") ../output/boot/"kernel-${KERNEL_PREFIX}-${ARCH}-${PACKAGE_VERSION}-${SUFFIX}"
+   cp -rfv $(readlink -f "arch/${ARCH}/boot/bzImage") ../output/boot/"kernel-${KERNEL_PREFIX}-${ARCH}-${kernel_version}-${SUFFIX}"
 else
-   cp -rfv arch/${ARCH}/boot/bzImage ../output/boot/"kernel-${KERNEL_PREFIX}-${ARCH}-${PACKAGE_VERSION}-${SUFFIX}"
+   cp -rfv arch/${ARCH}/boot/bzImage ../output/boot/"kernel-${KERNEL_PREFIX}-${ARCH}-${kernel_version}-${SUFFIX}"
 fi
