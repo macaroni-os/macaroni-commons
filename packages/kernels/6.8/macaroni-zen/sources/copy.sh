26430da2383017aaeadee2d31eaf1a30b34796f8
Daniele Rondina <geaaru@macaronios.org>
2025-09-13 19:40:59 +0200

    kernel-6.8/macaroni-zen: Removed (EOL)

--- packages/kernels/6.8/macaroni-zen/sources/copy.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-set -ex
-PACKAGE_VERSION=${PACKAGE_VERSION%\+*}
-kernel_version="${PACKAGE_VERSION}-zen1"
-mkdir -p /luetbuild/sources/usr/src/
-mv ${KERNEL_TYPE} /luetbuild/sources/usr/src/${KERNEL_TYPE}-${kernel_version}-${SUFFIX}
